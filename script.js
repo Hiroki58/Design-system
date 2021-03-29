cb52d29468fde6137b6229d624644b3497a76c3e
Hiroki <gnarly.hiro.15@gmail.com>
2021-03-29 12:54:03 +0900

    font selector add

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 
 function changeColor() {
-    let v1 = document.getElementById("H");
-    let v2 = document.getElementById("S");
-    let v3 = document.getElementById("L");
+  let v1 = document.getElementById("H");
+  let v2 = document.getElementById("S");
+  let v3 = document.getElementById("L");
+
+  hsl = `hsl(${v1.value},${v2.value}%,${v3.value}%)`
+
+  document.querySelector('.box').style.backgroundColor = hsl;
+
+}
   
-    hsl = `hsl(${v1.value},${v2.value}%,${v3.value}%)`
-  
-    document.querySelector('.box').style.backgroundColor = hsl;
-  
-  }
-    
-  function clearBtn(){
-      document.getElementById("H").value = "";
-    document.getElementById("S").value = "";
-    document.getElementById("L").value = "";
-  
-    document.querySelector('.box').style.backgroundColor = "white";
-  }  
-  
-  
-  
+function clearBtn(){
+    document.getElementById("H").value = "";
+  document.getElementById("S").value = "";
+  document.getElementById("L").value = "";
+
+  document.querySelector('.box').style.backgroundColor = "white";
+}  
+
+function changeFonts(){
+ let fonts = document.getElementById("fonts");
+ document.querySelector('.letter').style.fontFamily = fonts.value;
+}
+
+function clearBtn1(){
+    document.getElementById("fonts").value = "";
+
+  document.querySelector('.letter').style.fontFamily = "";
+}
